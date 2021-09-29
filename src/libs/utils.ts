a7a4833f694a30965ddb39b626bc11acf2c1bd4b
kokily <kokily@naver.com>
2021-09-30 06:26:25 +0900

    Upload Image(S3)

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,6 +1,10 @@
 import Joi, { SchemaLike } from 'joi';
 import { Context } from 'koa';
+import aws from 'aws-sdk';
+import fs from 'fs';
+import moment from 'moment';
 
+// Validate Schema
 export function validateBody(ctx: Context, schema: SchemaLike) {
   const validation = Joi.valid(ctx.request.body, schema);
 
@@ -16,3 +20,54 @@ export function validateBody(ctx: Context, schema: SchemaLike) {
 
   return true;
 }
+
+// Upload Image
+export type FileType = {
+  name: string;
+  path: string;
+  type: string;
+};
+
+export type S3ReturnType = {
+  key: string;
+  url: string;
+};
+
+export const uploadImage = async (file: FileType): Promise<S3ReturnType> => {
+  aws.config.update({
+    region: 'ap-northeast-2',
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  });
+
+  const s3 = new aws.S3({
+    apiVersion: '2006-03-01',
+  });
+
+  return new Promise((resolve, reject) => {
+    const Body = fs.createReadStream(file.path);
+
+    Body.on('error', function (err) {
+      reject(err);
+    });
+
+    s3.upload(
+      {
+        Bucket: 'image.dnkdream.com',
+        Body,
+        Key: `${moment().format('YYMMDD_HHmmss')}_${file.name.trim()}`,
+        ContentType: file.type,
+      },
+      (err: Error, data: aws.S3.ManagedUpload.SendData) => {
+        if (err) {
+          reject(err);
+        } else if (data) {
+          resolve({
+            key: data.Key,
+            url: data.Location,
+          });
+        }
+      }
+    );
+  });
+};
