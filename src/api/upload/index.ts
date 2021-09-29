5a87079bed8f95a56706fa54bc4d548c47012441
kokily <kokily@naver.com>
2021-09-30 06:28:35 +0900

    Image Upload API

---
+++ src/api/upload/index.ts
@@ -0,0 +1,20 @@
+import { Context, Next } from 'koa';
+import Router from 'koa-router';
+import { uploadImage } from '../../libs/utils';
+
+const upload = new Router();
+
+upload.post('/', async (ctx: Context, next: Next) => {
+  if (ctx.request.files) {
+    const file = ctx.request.files.file;
+
+    // @ts-ignore
+    const { key, url } = await uploadImage(file);
+
+    ctx.body = { key, url };
+  } else {
+    return next();
+  }
+});
+
+export default upload;
